e4ae99280de9ff12fd3f6bb562870823de5be523
yohan <rmvmf96@gmail.com>
2025-04-28 13:53:21 +0900

    novel create api추가

---
+++ athena/src/main/java/goorm/athena/domain/novel/controller/NovelController.java
@@ -0,0 +1,27 @@
+package goorm.athena.domain.novel.controller;
+
+
+import goorm.athena.domain.novel.dto.req.NovelCreateRequest;
+import goorm.athena.domain.novel.dto.res.NovelCreateResponse;
+import goorm.athena.domain.novel.service.NovelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/novels")
+public class NovelController {
+
+    private final NovelService novelService;
+
+    @PostMapping
+    public ResponseEntity<NovelCreateResponse> createNovel(@RequestBody NovelCreateRequest request) {
+        NovelCreateResponse response = novelService.createNovel(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ athena/src/main/java/goorm/athena/domain/novel/service/NovelService.java
@@ -0,0 +1,32 @@
+package goorm.athena.domain.novel.service;
+
+import goorm.athena.domain.novel.dto.req.NovelCreateRequest;
+import goorm.athena.domain.novel.dto.res.NovelCreateResponse;
+import goorm.athena.domain.novel.entity.Novel;
+import goorm.athena.domain.novel.repository.NovelRepository;
+import goorm.athena.domain.user.entity.User;
+import goorm.athena.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NovelService {
+
+    private final UserRepository userRepository;
+    private final NovelRepository novelRepository;
+
+    public NovelCreateResponse createNovel(NovelCreateRequest request) {
+
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid author ID"));
+
+        Novel novel = Novel.create(user, request);
+
+        Novel savedNovel = novelRepository.save(novel);
+
+        return new NovelCreateResponse(
+                savedNovel.getId()
+        );
+    }
+}
