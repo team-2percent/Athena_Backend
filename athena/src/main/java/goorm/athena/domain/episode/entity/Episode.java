537bfa9558e0e8c4f1ac46a6b4344fdca98f6340
goorm <rlarlgus300@gmail.com>
2025-04-28 10:15:06 +0900

    feat : 회차 엔티티 추가

--- athena/src/main/java/goorm/athena/domain/episode/entity/Episode.java
+++ athena/src/main/java/goorm/athena/domain/episode/entity/Episode.java
@@ -1,4 +1,26 @@
 package goorm.athena.domain.episode.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Episode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long episodeId;
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private int price;
+    private Long viewCount;
+
 }
