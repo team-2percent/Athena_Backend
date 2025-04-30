30b2f00d2cea318f2e8cb9ae41ea833413862371
yohan <rmvmf96@gmail.com>
2025-04-30 16:16:33 +0900

    스웨거,yml,예외처리 설정

--- athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
+++ athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package goorm.athena.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf().disable() // 임시로 csrf 비활성화
+                .authorizeHttpRequests((authorize) -> {
+                    authorize
+                            .requestMatchers("/api/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html") // Swagger UI 및 API 문서 경로 허용
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated();
+                })
+                /*
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll());
+                 */ .formLogin(form -> form.disable());
+        return http.build();
+    }
 }
