6c5d4bb29645fb972cd2897f90de90c0c9ca51d4
yohan <rmvmf96@gmail.com>
2025-04-30 16:16:33 +0900

    스웨거,yml,예외처리 설정

--- athena/src/main/java/goorm/athena/global/exception/ErrorCode.java
+++ athena/src/main/java/goorm/athena/global/exception/ErrorCode.java
@@ -1,4 +1,21 @@
 package goorm.athena.global.exception;
 
-public class ErrorCode {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    NOVEL_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 Novel이 존재하지 않습니다."),
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 Episode의 Page가 존재하지 않습니다."),
+    EPISODE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 Episode가 존재하지 않습니다.");
+
+    private final HttpStatus errorCode;
+    private final String errorMessage;
+
+    ErrorCode(HttpStatus errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
 }
