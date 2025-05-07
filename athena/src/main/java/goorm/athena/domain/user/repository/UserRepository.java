5f2a0f73814a5aa54f35b41dd937e603359af0e9
goorm <rlarlgus300@gmail.com>
2025-05-07 12:11:59 +0900

    refactor : 입력값이 null로 들어오는 것을 방지

--- athena/src/main/java/goorm/athena/domain/user/repository/UserRepository.java
+++ athena/src/main/java/goorm/athena/domain/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package goorm.athena.domain.user.repository;
 import goorm.athena.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
 
