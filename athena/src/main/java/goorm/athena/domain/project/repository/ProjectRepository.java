0ee049117d445b24d082b17e3866613abcd872b6
Jay(김재훈) <jkim94937@gmail.com>
2025-05-07 23:41:10 +0900

    [#16] 검색 기능 구현 (#34)
    * chore: Add `.gitignore` also to root level to prevent create directory named `.vscode`
    
    * chore: Add docker container to create MariaDB for our project
    
    * chore: Change MariaDB to MySQL 8.0
    
    * chore: Hide secrets from `docker-compose.yml`
    
    * feat: Done with first step implementing Search feature
    
    - Create dynamicall query by using JpaSpecification
      - It provides function with `product.title` or `user.seller_name` to search
      - It returns result by 20 items each page
      - It removes distinct results automatically
    
    Expected next step:
    - Test it manually
    - Write test code
    - Move on another issue
    
    * refactor: Separate Controller as between interface and implements
    
    - Swagger added controller's interface
    
    * chore: Add comment to Service file
    
    * chore: .coderabbitai.yaml 파일 추가
    
    코드 리뷰 및 자동화 도구 설정을 포함한 .coderabbitai.yaml 파일을 새로 추가했습니다. 이 파일은 다양한 도구와 기능을 활성화하여 코드 품질을 향상시키는 데 기여합니다.
    
    * feat: (코드래빗 제안사항 일부 적용)검색 기능 개선 및 유효성 검사 추가
    
    검색 목록 조회 API에 검색어 유효성 검사를 추가하고, 불필요한 SerachRequest 클래스를 삭제했습니다. 또한, Search 엔티티에 필요한 어노테이션을 추가하여 구조를 개선했습니다. SQL 인젝션 방지를 위한 검색어 처리 로직도 추가되었습니다.
    
    * chore: .coderabbitai.yaml 파일 수정
    
    불필요한 코드 리뷰 및 자동화 도구 설정을 제거하여 .coderabbitai.yaml 파일을 간소화했습니다. 이를 통해 코드 품질 관리에 필요한 도구를 더욱 명확하게 설정할 수 있게 되었습니다.
    
    * refactor: UserControllerImpl 클래스의 메서드 포맷 수정
    
    UserControllerImpl 클래스의 메서드 정의에서 불필요한 공백을 제거하고, 일관된 포맷을 적용하여 코드 가독성을 향상시켰습니다.
    
    * feat: 프로젝트 검색 기능 추가 및 관련 DTO, Mapper 구현
    
    프로젝트 검색 기능을 위한 ProjectRepository와 ProjectSpecification을 추가하였고, 검색 결과를 반환하기 위한 SearchResultResponse DTO와 SearchMapper를 구현했습니다. 또한, SearchController와 SearchService를 수정하여 새로운 검색 로직을 통합했습니다.
    
    * refactor: SearchController 및 SearchService의 메서드 수정, DTO 패키지 구조 변경, null 체크 추가로 안정성 향상
    
    * refactor: SearchMapper에서 null 체크 제거 및 코드 간소화

---
+++ athena/src/main/java/goorm/athena/domain/project/repository/ProjectRepository.java
@@ -0,0 +1,8 @@
+package goorm.athena.domain.project.repository;
+
+import goorm.athena.domain.project.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
+}
---
+++ athena/src/main/java/goorm/athena/domain/search/controller/SearchController.java
@@ -0,0 +1,27 @@
+package goorm.athena.domain.search.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import goorm.athena.domain.search.dto.response.SearchResultResponse;
+
+import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Size;
+
+@Tag(name = "Search", description = "검색 관련 API")
+@RequestMapping("/api/search")
+public interface SearchController {
+        @Operation(summary = "검색 목록 조회", description = "검색 목록을 조회합니다.<br>" +
+                        "검색은 기본적으로 상품명 또는 판매자명으로 검색되며, 필요한 정보는 searchWord 파라미터입니다.<br>" +
+                        "반환 결과는 상품 목록으로 한 페이지에 20개씩 반환됩니다.")
+        @ApiResponse(responseCode = "200", description = "검색 목록 조회 성공")
+        @GetMapping
+        // ToDo Page<Search> 타입은 추후 Product의 Response Dto 타입으로 변경 필요
+        ResponseEntity<SearchResultResponse> searchList(
+                        @RequestParam(defaultValue = "0") Integer page,
+                        @RequestParam(defaultValue = "") @Size(min = 1, message = "검색어는 최소 1글자 이상이어야 합니다.") String searchWord);
+}
