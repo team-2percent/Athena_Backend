162b2c407f3441051803df30643d0468aa582945
goorm <rlarlgus300@gmail.com>
2025-05-07 17:36:57 +0900

    refactor : refresh 토큰 재발행 시, 해당 사용자의 이전 모든 refreshKey 삭제하도록 구현

--- athena/src/main/java/goorm/athena/domain/user/entity/RefreshToken.java
+++ athena/src/main/java/goorm/athena/domain/user/entity/RefreshToken.java
@@ -16,7 +16,7 @@ public class RefreshToken {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     private String value;
