5248802cadc4e7826e11260c4ddf1a95fd4d7933
yohan <rmvmf96@gmail.com>
2025-05-08 00:56:42 +0900

    fix : 검색 기능 위한 프로젝트 레포지토리 코드 import 재 추가

--- athena/src/main/java/goorm/athena/domain/project/repository/ProjectRepository.java
+++ athena/src/main/java/goorm/athena/domain/project/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package goorm.athena.domain.project.repository;
 
 import goorm.athena.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
 }
