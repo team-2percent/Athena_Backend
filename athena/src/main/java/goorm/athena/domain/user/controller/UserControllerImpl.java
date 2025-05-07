7429841985290db5ec48edd6e717a9e7d82e36c8
Jay(김재훈) <jkim94937@gmail.com>
2025-05-07 14:31:21 +0900

    refactor: UserControllerImpl 클래스의 메서드 포맷 수정
    UserControllerImpl 클래스의 메서드 정의에서 불필요한 공백을 제거하고, 일관된 포맷을 적용하여 코드 가독성을 향상시켰습니다.

--- athena/src/main/java/goorm/athena/domain/user/controller/UserControllerImpl.java
+++ athena/src/main/java/goorm/athena/domain/user/controller/UserControllerImpl.java
@@ -6,7 +6,6 @@ import goorm.athena.domain.user.dto.response.UserCreateResponse;
 import goorm.athena.domain.user.dto.response.UserGetResponse;
 import goorm.athena.domain.user.dto.response.UserUpdateResponse;
 import goorm.athena.domain.user.service.UserService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,34 +13,34 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
-public class UserControllerImpl implements UserController{
+public class UserControllerImpl implements UserController {
 
     private final UserService userService;
 
     @Override
     @PostMapping
-    public ResponseEntity<UserCreateResponse> createUser(@RequestBody UserCreateRequest request){
+    public ResponseEntity<UserCreateResponse> createUser(@RequestBody UserCreateRequest request) {
         UserCreateResponse response = userService.createUser(request);
         return ResponseEntity.ok(response);
     }
 
     @Override
     @PutMapping
-    public ResponseEntity<UserUpdateResponse> updateUser(@RequestBody UserUpdateRequest request){
+    public ResponseEntity<UserUpdateResponse> updateUser(@RequestBody UserUpdateRequest request) {
         UserUpdateResponse response = userService.updateUser(request);
         return ResponseEntity.ok(response);
     }
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<UserGetResponse> getUserById(@PathVariable Long id){
+    public ResponseEntity<UserGetResponse> getUserById(@PathVariable Long id) {
         UserGetResponse response = userService.getUserById(id);
         return ResponseEntity.ok(response);
     }
 
     @Override
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
