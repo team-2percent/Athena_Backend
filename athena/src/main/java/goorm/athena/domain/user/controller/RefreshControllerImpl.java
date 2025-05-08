eb41d65c44d72c366bc69163d51f40923e7d8191
goorm <rlarlgus300@gmail.com>
2025-05-08 09:43:10 +0900

    refactor : refresh 토큰을 갱신하는 과정에서 access를 인증 헤더에서 받아와 변환하도록 처리

--- athena/src/main/java/goorm/athena/domain/user/controller/RefreshControllerImpl.java
+++ athena/src/main/java/goorm/athena/domain/user/controller/RefreshControllerImpl.java
@@ -4,6 +4,7 @@ import goorm.athena.domain.user.dto.response.RefreshTokenResponse;
 import goorm.athena.domain.user.service.RefreshTokenService;
 import goorm.athena.global.exception.CustomException;
 import goorm.athena.global.exception.ErrorCode;
+import goorm.athena.global.jwt.util.JwtTokenizer;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/refreshToken")
 public class RefreshControllerImpl implements RefreshController{
     private final RefreshTokenService refreshTokenService;
+    private final JwtTokenizer jwtTokenizer;
 
     @Override
     @PostMapping("/ReissueRefresh")
     public ResponseEntity<RefreshTokenResponse> requestRefresh(@CookieValue("refreshToken") String refreshToken,
-                                                               @RequestBody String accessToken,
+                                                               @RequestHeader("Authorization") String header,
                                                                HttpServletResponse response){
+        String accessToken = jwtTokenizer.extractBearerToken(header);
 
         RefreshTokenResponse refreshResponse = refreshTokenService.reissueToken(accessToken, refreshToken, response);
         return ResponseEntity.ok(refreshResponse);
