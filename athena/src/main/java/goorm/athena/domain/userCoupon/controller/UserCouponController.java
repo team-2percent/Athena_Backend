67c67eba9835a3810999debae9723269ec4a34a5
goorm <rlarlgus300@gmail.com>
2025-05-08 19:03:08 +0900

    feat : 유저 쿠폰 발급 시 해당 쿠폰의 재고가 감소하는 Api 구현 및 중복 검증 구현

--- athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponController.java
+++ athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponController.java
@@ -3,7 +3,11 @@ package goorm.athena.domain.userCoupon.controller;
 import goorm.athena.domain.userCoupon.dto.req.UserCouponCreateRequest;
 import goorm.athena.domain.userCoupon.dto.res.UserCouponCreateResponse;
 import goorm.athena.domain.userCoupon.entity.UserCoupon;
+import goorm.athena.global.jwt.util.CheckLogin;
+import goorm.athena.global.jwt.util.LoginUserRequest;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,5 +23,6 @@ public interface UserCouponController {
     @Operation(summary = "유저 쿠폰 생성 API", description = "해당 쿠폰을 유저에게 발급합니다.")
     @ApiResponse(responseCode = "200", description = "유저의 새 쿠폰이 생성되었습니다.",
         content = @Content(schema = @Schema(implementation = UserCoupon.class)))
-    ResponseEntity<UserCouponCreateResponse> createUserCoupon(@RequestBody UserCouponCreateRequest request);
+    public ResponseEntity<UserCouponCreateResponse> issueCoupon(@Parameter(hidden = true) @CheckLogin LoginUserRequest loginUserRequest,
+                                                                @RequestBody UserCouponCreateRequest request);
 }
--- athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponControllerImpl.java
+++ athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponControllerImpl.java
@@ -3,6 +3,8 @@ package goorm.athena.domain.userCoupon.controller;
 import goorm.athena.domain.userCoupon.dto.req.UserCouponCreateRequest;
 import goorm.athena.domain.userCoupon.dto.res.UserCouponCreateResponse;
 import goorm.athena.domain.userCoupon.service.UserCouponService;
+import goorm.athena.global.jwt.util.CheckLogin;
+import goorm.athena.global.jwt.util.LoginUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +20,9 @@ public class UserCouponControllerImpl implements UserCouponController {
 
     @Override
     @PostMapping
-    public ResponseEntity<UserCouponCreateResponse> createUserCoupon(@RequestBody UserCouponCreateRequest request){
-        UserCouponCreateResponse response = userCouponService.createUserCoupon(request);
+    public ResponseEntity<UserCouponCreateResponse> issueCoupon(@CheckLogin LoginUserRequest loginUserRequest,
+                                                                @RequestBody UserCouponCreateRequest request){
+        UserCouponCreateResponse response = userCouponService.issueCoupon(loginUserRequest.userId(), request);
         return ResponseEntity.ok(response);
     }
 }
