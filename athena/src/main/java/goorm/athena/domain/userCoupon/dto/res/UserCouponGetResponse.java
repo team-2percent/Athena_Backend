4203bb94e5e920dc5bc0cf68429849d67de18e39
goorm <rlarlgus300@gmail.com>
2025-05-10 00:53:26 +0900

    setting : 유저 보유 쿠폰 조회 응답 dto 속성 추가

--- athena/src/main/java/goorm/athena/domain/userCoupon/dto/res/UserCouponGetResponse.java
+++ athena/src/main/java/goorm/athena/domain/userCoupon/dto/res/UserCouponGetResponse.java
@@ -5,6 +5,8 @@ import goorm.athena.domain.userCoupon.entity.Status;
 import java.time.LocalDateTime;
 
 public record UserCouponGetResponse(
+        Long id,
+        Long couponId,
         String title,
         String content,
         int price,
