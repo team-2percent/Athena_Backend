92ec8afe1c8422772d0ee1dc24bccf568a1e3d36
goorm <rlarlgus300@gmail.com>
2025-05-10 00:54:42 +0900

    fix : Swagger 테스트 시 dto로 매개 변수를 받아야 Swagger가 인식하여 그에 맞게 매개 변수 수정 및 유저 쿠폰 생성 Swagger 설명 추가

--- athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponController.java
+++ athena/src/main/java/goorm/athena/domain/userCoupon/controller/UserCouponController.java
@@ -1,6 +1,8 @@
 package goorm.athena.domain.userCoupon.controller;
 
+import goorm.athena.domain.couponEvent.dto.res.CouponEventGetResponse;
 import goorm.athena.domain.userCoupon.dto.req.UserCouponIssueRequest;
+import goorm.athena.domain.userCoupon.dto.req.UserCouponUseRequest;
 import goorm.athena.domain.userCoupon.dto.res.UserCouponGetResponse;
 import goorm.athena.domain.userCoupon.dto.res.UserCouponIssueResponse;
 import goorm.athena.domain.userCoupon.entity.UserCoupon;
@@ -24,7 +26,8 @@ import java.util.List;
 @RequestMapping("/api/userCoupon")
 public interface UserCouponController {
 
-    @Operation(summary = "유저 쿠폰 생성 API", description = "해당 쿠폰을 유저에게 발급합니다.")
+    @Operation(summary = "유저 쿠폰 생성 API", description = "해당 쿠폰을 유저에게 발급합니다.<br>" +
+            "발급받을 쿠폰의 상태가 '발급 중' 상태가 아니거나 발급받은 적 있을 경우 해당 쿠폰을 발급받을 수 없습니다.")
     @ApiResponse(responseCode = "200", description = "유저의 새 쿠폰이 생성되었습니다.",
         content = @Content(schema = @Schema(implementation = UserCoupon.class)))
     @PostMapping
@@ -36,7 +39,7 @@ public interface UserCouponController {
     @ApiResponse(responseCode = "203", description = "해당 유저가 보유한 쿠폰을 사용했습니다.")
     @PostMapping
     public ResponseEntity<Void> useCoupon(@Parameter(hidden = true) @CheckLogin LoginUserRequest loginUserRequest,
-                                          @RequestBody Long userCouponId);
+                                          @RequestBody UserCouponUseRequest request);
 
     @Operation(summary = "유저 쿠폰 만료 스케줄러 수동 API", description = "쿠폰의 만료일을 기준으로 모든 유저의 쿠폰들의 상태를 수동 만료 처리합니다.<br>" +
             "어떤 상태이든 상관없이, 만료일이 됐을 경우 모두 상태를 만료처리 합니다.")
@@ -46,6 +49,8 @@ public interface UserCouponController {
 
     @Operation(summary = "유저 쿠폰 조회 API", description = "유저가 현재 보유중인 모든 쿠폰을 조회합니다.<br>" +
             "모든 쿠폰을 조회하기 때문에 추후 만료 처리를 뺀 쿠폰들을 모두 조회할 수 있습니다.")
+    @ApiResponse(responseCode = "200", description = "유저가 보유하는 모든 쿠폰들을 조회했습니다.",
+            content = @Content(schema = @Schema(implementation = UserCouponGetResponse.class)))
     @GetMapping
     public ResponseEntity<List<UserCouponGetResponse>> getUserCoupon(@Parameter(hidden = true) @CheckLogin LoginUserRequest loginUserRequest);
 
