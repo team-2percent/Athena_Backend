ba9541c7daae00b7a290511d16144df1cb1dcb34
goorm <rlarlgus300@gmail.com>
2025-05-09 12:58:59 +0900

    feat : 관리자 권한을 가진 사용자만 쿠폰 및 쿠폰 이벤트를 등록하는 접근제어 적용

--- athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
+++ athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import goorm.athena.global.jwt.provider.JwtAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,6 +33,9 @@ public class SecurityConfig {
                                                    AuthenticationManager authenticationManager) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/api/coupon/**", "/api/couponEvent/**")
+                .hasRole("ADMIN")
+
                 .requestMatchers(
                         "/api/payment/**",
                         "/api/**",
