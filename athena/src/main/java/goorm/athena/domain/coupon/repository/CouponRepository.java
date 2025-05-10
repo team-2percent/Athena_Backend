1fe8d525cbf3bce59127cd444d042bee7d87adae
goorm <rlarlgus300@gmail.com>
2025-05-10 19:57:13 +0900

    refafctor : 쿠폰 스케줄러 상태 전환 서비스에서 쿼리가 4번 발생하던 점에서 2번으로 리팩터링

--- athena/src/main/java/goorm/athena/domain/coupon/repository/CouponRepository.java
+++ athena/src/main/java/goorm/athena/domain/coupon/repository/CouponRepository.java
@@ -3,6 +3,7 @@ package goorm.athena.domain.coupon.repository;
 import goorm.athena.domain.coupon.entity.Coupon;
 import goorm.athena.domain.coupon.entity.CouponStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +14,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
     List<Coupon> findByCouponStatusAndStartAtLessThanEqual(CouponStatus status, LocalDateTime date);
 
     List<Coupon> findByCouponStatusNotAndEndAtLessThan(CouponStatus status, LocalDateTime dateTime);
+
+    @Query("SELECT c FROM Coupon c WHERE c.startAt <= :now OR c.endAt <= :now")
+    List<Coupon> findCouponsToUpdate(LocalDateTime now);
 }
