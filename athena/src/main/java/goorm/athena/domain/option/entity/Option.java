e0c2ad4b6266ab6b0015bb574499262ffa6b77a2
Hyerim Jeon <jshsn0806@naver.com>
2025-05-14 08:58:34 +0900

    feat: Option 생성 매소드 추가

--- athena/src/main/java/goorm/athena/domain/option/entity/Option.java
+++ athena/src/main/java/goorm/athena/domain/option/entity/Option.java
@@ -2,12 +2,15 @@ package goorm.athena.domain.option.entity;
 
 import goorm.athena.domain.product.entity.Product;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "options")
 public class Option {
     @Id
@@ -19,5 +22,11 @@ public class Option {
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Product product;
 
-    private String optionName;  // 옵션명
+    private String optionName;        // 옵션명
+
+    @Builder
+    public Option(Product product, String optionName) {
+        this.product = product;
+        this.optionName = optionName;
+    }
 }
