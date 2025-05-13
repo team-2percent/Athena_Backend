c3407bd2faece9d3f7fe3016484b1fe4028a95c3
yohan <rmvmf96@gmail.com>
2025-05-13 14:39:24 +0900

    feat : 스케줄링 예외처리 추가

--- athena/src/main/java/goorm/athena/domain/settlement/scheduler/SettlementScheduler.java
+++ athena/src/main/java/goorm/athena/domain/settlement/scheduler/SettlementScheduler.java
@@ -4,9 +4,11 @@ import goorm.athena.domain.settlement.service.SettlementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+import lombok.extern.slf4j.Slf4j;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SettlementScheduler {
@@ -14,6 +16,11 @@ public class SettlementScheduler {
 
     @Scheduled(cron = "0 0 3 1 * ?") // 매월 1일 오전 3시
     public void autoSettlement() {
-        settlementService.executeMonthlySettlement(LocalDate.now());
+        try {
+            settlementService.executeMonthlySettlement(LocalDate.now(ZoneId.of("Asia/Seoul")));
+            log.info(" 월 정산 스케줄러 정상 실행 완료");
+        } catch (Exception e) {
+            log.error(" 월 정산 스케줄러 실행 중 오류 발생", e);
+        }
     }
 }
