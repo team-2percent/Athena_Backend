17564f996dc68cfbb71d106dff77cbffb980f6d3
Hyerim Jeon <jshsn0806@naver.com>
2025-05-14 09:02:19 +0900

    feat: 상품, 옵션 생성 및 삭제 메소드 추가

--- athena/src/main/java/goorm/athena/domain/project/mapper/ProjectMapper.java
+++ athena/src/main/java/goorm/athena/domain/project/mapper/ProjectMapper.java
@@ -3,12 +3,14 @@ package goorm.athena.domain.project.mapper;
 import goorm.athena.domain.category.entity.Category;
 import goorm.athena.domain.imageGroup.entity.ImageGroup;
 import goorm.athena.domain.project.dto.req.ProjectCreateRequest;
+import goorm.athena.domain.project.dto.req.ProjectUpdateRequest;
 import goorm.athena.domain.project.dto.res.ProjectIdResponse;
 import goorm.athena.domain.project.entity.Project;
 import goorm.athena.domain.user.entity.User;
 
 public class ProjectMapper {
     // ProjectCreateRequest(Dto) -> Entity
+
     public static Project toEntity(ProjectCreateRequest request, User seller, ImageGroup imageGroup, Category category) {
         return Project.builder()
                 .seller(seller)
@@ -32,4 +34,6 @@ public class ProjectMapper {
                 .imageGroupId(project.getImageGroup().getId())
                 .build();
     }
+
+
 }
