44149a7af9519fb60f646e1bbb863fd124efc2f7
Jay(김재훈) <jkim94937@gmail.com>
2025-05-23 17:23:12 +0900

    feat: Add image server into Spring Boot

--- athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
+++ athena/src/main/java/goorm/athena/global/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                         "/api/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
-                        "/swagger-ui.html")
+                        "/swagger-ui.html",
+                        "/images/**")
                 .permitAll()
                 .anyRequest().authenticated());
 
