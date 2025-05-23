eee46505a8792a3c53fe66e9b66d44ee90252bd6
goorm <rlarlgus300@gmail.com>
2025-05-23 16:53:27 +0900

    feat : 후기 입력값 검증 추가

--- athena/src/main/java/goorm/athena/domain/comment/dto/req/CommentCreateRequest.java
+++ athena/src/main/java/goorm/athena/domain/comment/dto/req/CommentCreateRequest.java
@@ -1,6 +1,10 @@
 package goorm.athena.domain.comment.dto.req;
 
+import jakarta.persistence.Column;
+
 public record CommentCreateRequest(
         Long projectId,
+
+        @Column(length = 1000, nullable = false)
         String content
 ) { }
