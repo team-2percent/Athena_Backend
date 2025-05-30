d1c362573f028efff2c11fe29d706e304f06ea4e
Jay(김재훈) <jkim94937@gmail.com>
2025-05-30 15:11:22 +0900

    feat: FirebaseConfig에서 환경 변수를 사용하여 서비스 계정 JSON 생성 로직 개선

--- athena/src/main/java/goorm/athena/global/config/FirebaseConfig.java
+++ athena/src/main/java/goorm/athena/global/config/FirebaseConfig.java
@@ -8,19 +8,45 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
-
-    @Value("${fcm.certification}")
-    private String firebaseConfigFilePath;
+    @Value("${FIREBASE_PROJECT_ID}")
+    private String firebaseProjectId;
+    @Value("${FIREBASE_PRIVATE_KEY_ID}")
+    private String firebasePrivateKeyId;
+    @Value("${FIREBASE_PRIVATE_KEY}")
+    private String firebasePrivateKey;
+    @Value("${FIREBASE_CLIENT_EMAIL}")
+    private String firebaseClientEmail;
+    @Value("${FIREBASE_CLIENT_ID}")
+    private String firebaseClientId;
 
     @PostConstruct
     public void init() {
-        try (FileInputStream serviceAccount = new FileInputStream(firebaseConfigFilePath)) {
+        try {
+            String json = String.format(
+                    "{\n" +
+                            "  \"type\": \"service_account\",\n" +
+                            "  \"project_id\": \"%s\",\n" +
+                            "  \"private_key_id\": \"%s\",\n" +
+                            "  \"private_key\": \"%s\",\n" +
+                            "  \"client_email\": \"%s\",\n" +
+                            "  \"client_id\": \"%s\",\n" +
+                            "  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n" +
+                            "  \"token_uri\": \"https://oauth2.googleapis.com/token\",\n" +
+                            "  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n" +
+                            "  \"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/%s\",\n" +
+                            "  \"universe_domain\": \"googleapis.com\"\n" +
+                        "}",
+                    firebaseProjectId, firebasePrivateKeyId, firebasePrivateKey, firebaseClientEmail, firebaseClientId);
+
+            ByteArrayInputStream serviceAccount = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
