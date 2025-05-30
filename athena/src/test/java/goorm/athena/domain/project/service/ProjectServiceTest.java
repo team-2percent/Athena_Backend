be1ea24d3cfcf5e99f8f9e20cb472008e66637fa
Jay(김재훈) <jkim94937@gmail.com>
2025-05-30 13:44:08 +0900

    test: ProjectService에 대한 test 파일 생성

---
+++ athena/src/test/java/goorm/athena/domain/project/service/ProjectServiceTest.java
@@ -0,0 +1,75 @@
+package goorm.athena.domain.project.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ProjectServiceTest {
+  @Test
+  void testCreateProject() {
+
+  }
+
+  @Test
+  void testDeleteProject() {
+
+  }
+
+  @Test
+  void testGetById() {
+
+  }
+
+  @Test
+  void testGetEligibleProjects() {
+
+  }
+
+  @Test
+  void testGetProjectDetail() {
+
+  }
+
+  @Test
+  void testGetProjects() {
+
+  }
+
+  @Test
+  void testGetProjectsByCategory() {
+
+  }
+
+  @Test
+  void testGetProjectsByDeadLine() {
+
+  }
+
+  @Test
+  void testGetProjectsByNew() {
+
+  }
+
+  @Test
+  void testGetTopView() {
+
+  }
+
+  @Test
+  void testGetTopViewByPlan() {
+
+  }
+
+  @Test
+  void testSearchProjects() {
+
+  }
+
+  @Test
+  void testUpdateApprovalStatus() {
+
+  }
+
+  @Test
+  void testUpdateProject() {
+
+  }
+}
