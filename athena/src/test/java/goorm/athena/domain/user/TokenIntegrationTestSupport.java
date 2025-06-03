5e7a9912059b92c4a0be545e18c7e2e4b4e7546f
goorm <rlarlgus300@gmail.com>
2025-06-03 23:39:24 +0900

    refactor : 토큰 발급 테스트 Mock이 아닌 실제 객체를 연결하여 테스트하도록 리팩터링

--- athena/src/test/java/goorm/athena/domain/user/TokenIntegrationTestSupport.java
+++ athena/src/test/java/goorm/athena/domain/user/TokenIntegrationTestSupport.java
@@ -1,19 +1,35 @@
 package goorm.athena.domain.user;
 
+import goorm.athena.domain.imageGroup.entity.ImageGroup;
+import goorm.athena.domain.imageGroup.entity.Type;
+import goorm.athena.domain.imageGroup.service.ImageGroupService;
+import goorm.athena.domain.user.entity.User;
+import goorm.athena.domain.user.repository.UserRepository;
 import goorm.athena.domain.user.service.TokenService;
+import goorm.athena.domain.user.service.UserService;
 import goorm.athena.global.jwt.util.JwtTokenizer;
 import goorm.athena.util.IntegrationTestSupport;
+import goorm.athena.util.TestEntityFactory;
 import jakarta.servlet.http.HttpServletResponse;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
 
 public abstract class TokenIntegrationTestSupport extends IntegrationTestSupport {
-    @Mock
-    protected JwtTokenizer jwtTokenizer;
 
-    @Mock
-    protected HttpServletResponse response;
-
-    @InjectMocks
+    @Autowired
     protected TokenService tokenService;
+
+    @Autowired
+    protected ImageGroupService imageGroupService;
+
+    protected ImageGroup setupImageGroup() {
+        return imageGroupService.createImageGroup(Type.PROJECT);
+    }
+
+    protected User setupUser(String email, String password, String nickname, ImageGroup imageGroup) {
+        User user = TestEntityFactory.createUser(email, password, nickname, imageGroup);
+        return user;
+    }
 }
