0770b8068188107cecad9f6fe6d93fa8917a4d1a
goorm <rlarlgus300@gmail.com>
2025-06-03 23:38:03 +0900

    refactor : myInfo 테스트 mock이 아닌 실제 객체간 연결하여 테스트하도록 리팩터링

--- athena/src/test/java/goorm/athena/domain/user/MyInfoIntegrationTestSupport.java
+++ athena/src/test/java/goorm/athena/domain/user/MyInfoIntegrationTestSupport.java
@@ -1,21 +1,151 @@
 package goorm.athena.domain.user;
 
+import goorm.athena.domain.bankaccount.entity.BankAccount;
+import goorm.athena.domain.bankaccount.repository.BankAccountRepository;
+import goorm.athena.domain.category.entity.Category;
+import goorm.athena.domain.category.repository.CategoryRepository;
+import goorm.athena.domain.deliveryinfo.entity.DeliveryInfo;
+import goorm.athena.domain.deliveryinfo.repository.DeliveryInfoRepository;
+import goorm.athena.domain.imageGroup.entity.ImageGroup;
+import goorm.athena.domain.imageGroup.entity.Type;
+import goorm.athena.domain.imageGroup.repository.ImageGroupRepository;
+import goorm.athena.domain.imageGroup.service.ImageGroupService;
+import goorm.athena.domain.order.entity.Order;
+import goorm.athena.domain.order.repository.OrderRepository;
+import goorm.athena.domain.orderitem.entity.OrderItem;
+import goorm.athena.domain.orderitem.repository.OrderItemRepository;
+import goorm.athena.domain.product.entity.Product;
+import goorm.athena.domain.product.repository.ProductRepository;
+import goorm.athena.domain.project.entity.PlanName;
+import goorm.athena.domain.project.entity.PlatformPlan;
+import goorm.athena.domain.project.entity.Project;
+import goorm.athena.domain.project.repository.PlatformPlanRepository;
+import goorm.athena.domain.project.repository.ProjectRepository;
 import goorm.athena.domain.user.dto.response.MyProjectScrollResponse;
+import goorm.athena.domain.user.entity.User;
 import goorm.athena.domain.user.mapper.MyProjectScrollResponseMapper;
 import goorm.athena.domain.user.repository.MyInfoQueryRepository;
+import goorm.athena.domain.user.repository.UserRepository;
 import goorm.athena.domain.user.service.MyInfoService;
 import goorm.athena.util.IntegrationTestSupport;
+import goorm.athena.util.TestEntityFactory;
+import org.aspectj.weaver.ast.Or;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
+
+import java.time.LocalDateTime;
 
 public abstract class MyInfoIntegrationTestSupport extends IntegrationTestSupport {
 
-    @Mock
-    protected MyInfoQueryRepository myInfoQueryRepository;
-
-    @Mock
-    protected MyProjectScrollResponseMapper myProjectScrollResponseMapper;
-
-    @InjectMocks
+    @Autowired
     protected MyInfoService myInfoService;
+
+    @Autowired
+    protected ProjectRepository projectRepository;
+
+    @Autowired
+    protected CategoryRepository categoryRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected ImageGroupService imageGroupService;
+
+    @Autowired
+    protected ImageGroupRepository imageGroupRepository;
+
+    @Autowired
+    protected BankAccountRepository bankAccountRepository;
+
+    @Autowired
+    protected PlatformPlanRepository platformPlanRepository;
+
+    @Autowired
+    protected OrderItemRepository orderItemRepository;
+
+    @Autowired
+    protected OrderRepository orderRepository;
+
+    @Autowired
+    protected ProductRepository productRepository;
+
+    @Autowired
+    protected DeliveryInfoRepository deliveryInfoRepository;
+
+    @BeforeEach
+    void setUp() {
+        orderItemRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        deliveryInfoRepository.deleteAllInBatch();
+        projectRepository.deleteAllInBatch();
+        platformPlanRepository.deleteAllInBatch();
+        bankAccountRepository.deleteAllInBatch();
+        categoryRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+        imageGroupRepository.deleteAllInBatch();
+    }
+
+    protected ImageGroup setupImageGroup() {
+        return imageGroupService.createImageGroup(Type.PROJECT);
+    }
+
+    protected User setupUser(String email, String password, String nickname, ImageGroup imageGroup) {
+        User user = TestEntityFactory.createUser(email, password, nickname, imageGroup);
+        return user;
+    }
+
+    protected Category setupCategory(String categoryName) {
+        Category category = TestEntityFactory.createCategory(categoryName);
+        return category;
+    }
+
+    protected BankAccount setupBankAccount(User user, String accountNumber, String accountHolder, String bankName, boolean isDefault) {
+        BankAccount bankAccount = TestEntityFactory.createBankAccount(user, accountNumber, accountHolder, bankName, isDefault);
+        return bankAccount;
+    }
+
+    protected PlatformPlan setupPlatformPlan(PlanName planName, int platformFeeRate, int pgFeeRate, int vatRate, String description) {
+        PlatformPlan plan = TestEntityFactory.createPlatformPlan(planName, platformFeeRate, pgFeeRate, vatRate, description);
+        return plan;
+    }
+
+    protected Project setupProject(User user, Category category, ImageGroup imageGroup,
+                                 BankAccount bankAccount, PlatformPlan platformPlan,
+                                   String title, String description, Long goalAmount, Long totalAmount) {
+        Project project = TestEntityFactory.createProject(
+                user, category, imageGroup, bankAccount, platformPlan,
+                title, description, goalAmount, totalAmount
+        );
+        return project;
+    }
+
+    protected Product setupProduct(Project project, String name, String description, Long price, Long stock){
+        Product product = TestEntityFactory.createProduct(
+                project, name, description, price, stock);
+
+        return product;
+    }
+
+    protected DeliveryInfo setupDeliveryInfo(User user, String zipcode, String address, String detailAddress, boolean isDefault){
+        DeliveryInfo deliveryInfo = TestEntityFactory.createDeliveryInfo(user, zipcode, address, detailAddress, isDefault);
+
+        return deliveryInfo;
+    }
+
+    protected OrderItem setupOrderItem(Order order, Product product, int quantity, Long price){
+        OrderItem orderItem = TestEntityFactory.createOrderItem(order, product, quantity, price);
+
+        return orderItem;
+    }
+
+    protected Order setupOrder(User user, DeliveryInfo delivery, Project project, LocalDateTime orderedAt){
+        Order order = TestEntityFactory.createOrder(user, delivery, project, orderedAt);
+
+        return order;
+    }
 }
--- athena/src/test/java/goorm/athena/domain/user/service/MyInfoServiceTest.java
+++ athena/src/test/java/goorm/athena/domain/user/service/MyInfoServiceTest.java
@@ -1,25 +1,28 @@
 package goorm.athena.domain.user.service;
 
+import goorm.athena.domain.bankaccount.entity.BankAccount;
+import goorm.athena.domain.category.entity.Category;
+import goorm.athena.domain.deliveryinfo.entity.DeliveryInfo;
+import goorm.athena.domain.imageGroup.entity.ImageGroup;
+import goorm.athena.domain.order.entity.Order;
+import goorm.athena.domain.orderitem.entity.OrderItem;
+import goorm.athena.domain.product.entity.Product;
+import goorm.athena.domain.project.entity.PlanName;
+import goorm.athena.domain.project.entity.PlatformPlan;
 import goorm.athena.domain.project.entity.Project;
 import goorm.athena.domain.user.MyInfoIntegrationTestSupport;
-import goorm.athena.domain.user.UserIntegrationTestSupport;
 import goorm.athena.domain.user.dto.response.MyOrderScrollRequest;
 import goorm.athena.domain.user.dto.response.MyOrderScrollResponse;
 import goorm.athena.domain.user.dto.response.MyProjectScrollRequest;
 import goorm.athena.domain.user.dto.response.MyProjectScrollResponse;
+import goorm.athena.domain.user.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 class MyInfoServiceTest extends MyInfoIntegrationTestSupport{
 
@@ -27,86 +30,81 @@ class MyInfoServiceTest extends MyInfoIntegrationTestSupport{
     @Test
     void getMyProjects_Success() {
         // given
-        Long userId = 1L;
+        ImageGroup imageGroup = setupImageGroup();
+        User user = setupUser("test2@email.com", "1231231", "nickname2", imageGroup);
+        Category category = setupCategory("음식");
+        BankAccount bankAccount = setupBankAccount(user, "123" ,"123" ,"123", true);
+        PlatformPlan platformPlan = setupPlatformPlan(PlanName.BASIC, 10, 10, 10, "설명");
+        Project project = setupProject(user, category, imageGroup, bankAccount, platformPlan,
+                "프로젝2132132131트 제목", "설123213213명", 100000L, 10000L);
+
+        userRepository.save(user);
+        categoryRepository.save(category);
+        bankAccountRepository.save(bankAccount);
+        platformPlanRepository.save(platformPlan);
+        projectRepository.save(project);
+
         MyProjectScrollRequest request = new MyProjectScrollRequest(
                 LocalDateTime.now(), 10L, 5
         );
 
-        MyProjectScrollResponse rawResponse = new MyProjectScrollResponse(
-                List.of(new MyProjectScrollResponse.ProjectPreview(
-                        1L,
-                        "테스트 프로젝트",
-                        false,
-                        LocalDateTime.now(),
-                        LocalDateTime.now().plusDays(10),
-                        80L,
-                        "https://image.com/sample.jpg"
-                )),
-                LocalDateTime.now().plusDays(1),
-                2L
-        );
-
-        MyProjectScrollResponse mappedResponse = new MyProjectScrollResponse(
-                rawResponse.content(),
-                rawResponse.nextCursorValue(),
-                rawResponse.nextProjectId()
-        );
-
-        given(myInfoQueryRepository.findMyProjectsByCursor(
-                userId, request.nextCursorValue(), request.nextProjectId(), request.pageSize()))
-                .willReturn(rawResponse);
-
-        given(myProjectScrollResponseMapper.toResponse(
-                rawResponse.content(), rawResponse.nextCursorValue(), rawResponse.nextProjectId()))
-                .willReturn(mappedResponse);
-
         // when
-        MyProjectScrollResponse result = myInfoService.getMyProjects(userId, request);
+        MyProjectScrollResponse result = myInfoService.getMyProjects(user.getId(), request);
 
         // then
-        assertThat(result).isNotNull();
-        assertThat(result.content()).hasSize(1);
-        assertThat(result.content().get(0).title()).isEqualTo("테스트 프로젝트");
-        assertThat(result.nextProjectId()).isEqualTo(2L);
+        assertThat(result.content()).isNotEmpty();
+        assertThat(result.content().get(0).title()).isEqualTo("프로젝2132132131트 제목");
     }
 
     @DisplayName("로그인 한 유저의 내가 구매한 상품들을 성공적으로 무한 페이징 형식으로 조회한다.")
     @Test
     void getMyOrders_Success() {
         // given
-        Long userId = 1L;
+        ImageGroup imageGroup = setupImageGroup();
+        User user = setupUser("test@email.com", "123123", "nickname", imageGroup);
+        Category category = setupCategory("프로그래밍");
+        BankAccount bankAccount = setupBankAccount(user, "1243" ,"1243" ,"1243", true);
+        PlatformPlan platformPlan = setupPlatformPlan(PlanName.PRO, 11, 11, 11, "설명1");
+        Project project = setupProject(user, category, imageGroup, bankAccount, platformPlan,
+                "프로젝213213211트 제목", "설12313213명", 1000000L, 100000L);
+        DeliveryInfo deliveryInfo = setupDeliveryInfo(user, "12123123", "123123", "123213", true);
+        Product product = setupProduct(project, "123", "123", 12L, 12L);
+        Order order = setupOrder(user, deliveryInfo, project, LocalDateTime.now().minusDays(1));
+        Order order2 = setupOrder(user, deliveryInfo, project, LocalDateTime.now().minusDays(2));
+
+        OrderItem orderItem1 = setupOrderItem(order, product, 123, 12L);
+        OrderItem orderItem2 = setupOrderItem(order2, product, 123, 123L);
+        OrderItem orderItem3 = setupOrderItem(order2, product, 123, 123L);
+        OrderItem orderItem4 = setupOrderItem(order2, product, 123, 123L);
+
+        userRepository.save(user);
+        categoryRepository.save(category);
+        bankAccountRepository.save(bankAccount);
+        platformPlanRepository.save(platformPlan);
+        projectRepository.save(project);
+        deliveryInfoRepository.save(deliveryInfo);
+        productRepository.save(product);
+        orderRepository.saveAll(List.of(order, order2));
+        orderItemRepository.saveAll(List.of(orderItem1, orderItem2, orderItem3, orderItem4));
 
         LocalDateTime cursor = LocalDateTime.now().minusDays(1);
-        Long nextOrderId = 3L;
+        Long nextOrderId = 2L;
         int pageSize = 3;
-        MyOrderScrollRequest request = new MyOrderScrollRequest(cursor, nextOrderId, pageSize);
-
-        MyOrderScrollResponse.Item item = new MyOrderScrollResponse.Item(
-                1L, 1L, 1L, "프로젝트명", "상품명", "판매자명",
-                "https://image.com/sample.jpg",
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(10),
-                75L,
-                false
+        MyOrderScrollRequest request = new MyOrderScrollRequest(
+                cursor,
+                nextOrderId,
+                pageSize
         );
-
-        MyOrderScrollResponse expectedResponse = new MyOrderScrollResponse(
-                List.of(item),
-                LocalDateTime.now().plusDays(1),
-                2L
-        );
-
-        given(myInfoQueryRepository.findOrdersByUserIdWithScroll(
-                eq(userId), any(MyOrderScrollRequest.class)))
-                .willReturn(expectedResponse);
 
         // when
-        MyOrderScrollResponse result = myInfoService.getMyOrders(userId, request);
+        MyOrderScrollResponse result = myInfoService.getMyOrders(user.getId(), request);
 
         // then
         assertThat(result).isNotNull();
-        assertThat(result.content()).hasSize(1);
-        assertThat(result.content().get(0).projectName()).isEqualTo("프로젝트명");
-        assertThat(result.nextOrderId()).isEqualTo(2L);
+        assertThat(result.content()).hasSize(3);
+        MyOrderScrollResponse.Item firstItem = result.content().get(0);
+        assertThat(firstItem.projectName()).isEqualTo(project.getTitle());
+        assertThat(firstItem.sellerName()).isEqualTo(user.getNickname());
+        assertThat(firstItem.endAt()).isEqualTo(project.getEndAt());
     }
 }
