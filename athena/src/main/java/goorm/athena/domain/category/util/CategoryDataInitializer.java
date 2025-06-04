2a3a0cb4cf3225b030408e0453a4bbaf17697291
Jay(김재훈) <jkim94937@gmail.com>
2025-06-04 10:35:53 +0900

    feat: 카테고리 초기화 로직을 DefaultCategories 클래스로 분리하여 코드 정리

--- athena/src/main/java/goorm/athena/domain/category/util/CategoryDataInitializer.java
+++ athena/src/main/java/goorm/athena/domain/category/util/CategoryDataInitializer.java
@@ -1,12 +1,11 @@
 package goorm.athena.domain.category.util;
 
-import java.util.List;
-
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import goorm.athena.domain.category.repository.CategoryRepository;
+import goorm.athena.domain.category.util.DefaultCategories;
 
 @Component
 public class CategoryDataInitializer implements ApplicationRunner {
@@ -18,15 +17,6 @@ public class CategoryDataInitializer implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    List<String> defaultCategories = List.of(
-        "디지털",
-        "푸드",
-        "패션",
-        "생활",
-        "예술",
-        "출판",
-        "게임",
-        "기타");
-    defaultCategories.forEach(categoryRepository::saveIfNotExist);
+    DefaultCategories.VALUES.forEach(categoryRepository::saveIfNotExist);
   }
 }
