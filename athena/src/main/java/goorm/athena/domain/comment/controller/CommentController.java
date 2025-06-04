5283814d8a4206ee5d3af27bc2c50145c3a845e8
goorm <rlarlgus300@gmail.com>
2025-06-04 13:28:44 +0900

    refactor : 후기 작성 requestParam에서 requestBody로 변경

--- athena/src/main/java/goorm/athena/domain/comment/controller/CommentController.java
+++ athena/src/main/java/goorm/athena/domain/comment/controller/CommentController.java
@@ -12,10 +12,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +26,8 @@ public interface CommentController {
             content = @Content(schema = @Schema(implementation = CommentCreateResponse.class)))
     @PostMapping("/create")
     public ResponseEntity<CommentCreateResponse> createComment(@Parameter(hidden = true) @CheckLogin LoginUserRequest loginUserRequest,
-                                                               CommentCreateRequest commentCreateRequest);
+                                                               @RequestBody CommentCreateRequest commentCreateRequest);
+
     @Operation(summary = "댓글 조회 API", description = "해당 프로젝트 ID의 댓글들을 조회합니다.")
     @ApiResponse(responseCode = "200", description = "프로젝트의 댓글들을 조회했습니다.",
             content = @Content(schema = @Schema(implementation = CommentGetResponse.class)))
