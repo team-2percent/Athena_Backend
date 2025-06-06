0857b337192fc86f92ec826af620ae11fda95a3f
goorm <rlarlgus300@gmail.com>
2025-06-06 16:53:22 +0900

    test : BankAccount, DeliveryInfo Controller Autowired로 객체 간 연결하여 테스트 하는 방식으로 변경

--- athena/src/test/java/goorm/athena/domain/bankaccount/controller/BankAccountControllerImplTest.java
+++ athena/src/test/java/goorm/athena/domain/bankaccount/controller/BankAccountControllerImplTest.java
@@ -4,74 +4,112 @@ import goorm.athena.domain.bankaccount.BankAccountControllerIntegrationTestSuppo
 import goorm.athena.domain.bankaccount.dto.req.BankAccountCreateRequest;
 import goorm.athena.domain.bankaccount.dto.res.BankAccountCreateResponse;
 import goorm.athena.domain.bankaccount.dto.res.BankAccountGetResponse;
+import goorm.athena.domain.bankaccount.entity.BankAccount;
 import goorm.athena.domain.user.entity.Role;
+import goorm.athena.domain.user.entity.User;
 import goorm.athena.global.jwt.util.LoginUserRequest;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
 
+@Transactional
 class BankAccountControllerImplTest extends BankAccountControllerIntegrationTestSupport {
 
+    @Transactional
     @DisplayName("로그인 한 사용자가 자신의 계좌 정보를 생성한다.")
     @Test
     void createBankAccount() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
         BankAccountCreateRequest request = new BankAccountCreateRequest("123", "123", "123");
+
+        long oldSize = bankAccountRepository.count();
 
         // when
         ResponseEntity<BankAccountCreateResponse> response = controller.createBankAccount(loginUserRequest, request);
 
         // then
         assertEquals(200, response.getStatusCodeValue());
-        verify(bankAccountService).createBankAccount(loginUserRequest.userId(), request);
+
+        List<BankAccount> all = bankAccountRepository.findAll();
+        assertEquals(oldSize+1, all.size());
+        assertEquals("123", all.get(all.size()-1).getAccountHolder());
     }
 
+    @Transactional
     @DisplayName("유저의 기존에 저장된 계좌 정보와 선택한 계좌 정보의 상태를 변경한다.")
     @Test
     void changeAccountState() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
-        Long bankAccountId = 1L;
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+
+
+        BankAccount oldBankAccount = setupBankAccount(user, "123", "123", "123", true);
+        BankAccount newBankAccount = setupBankAccount(user, "123", "123", "123", false);
+
+        userRepository.save(user);
+        bankAccountRepository.saveAll(List.of(oldBankAccount, newBankAccount));
 
         // when
-        ResponseEntity<Void> response = controller.changeAccountState(loginUserRequest, bankAccountId);
+        ResponseEntity<Void> response = controller.changeAccountState(loginUserRequest, newBankAccount.getId());
 
         // then
-        assertEquals(204, response.getStatusCodeValue());
-        verify(bankAccountService).changeAccountState(loginUserRequest.userId(),bankAccountId);
+        assertThat(newBankAccount.isDefault()).isTrue();
+        assertThat(oldBankAccount.isDefault()).isFalse();
     }
 
+    @Transactional
+    @DisplayName("로그인 한 사용자가 자신이 선택한 계좌 정보를 삭제한다.")
     @Test
     void deleteBankAccount() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
-        Long id = 1L;
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+        BankAccount bankAccount = setupBankAccount(user, "!23", "123", "123", false);
+        BankAccount bankAccount2 = setupBankAccount(user, "!234", "124", "1243", false);
+        bankAccountRepository.saveAll(List.of(bankAccount, bankAccount2));
 
         // when
-        ResponseEntity<Void> response = controller.deleteBankAccount(loginUserRequest, id);
+        ResponseEntity<Void> response = controller.deleteBankAccount(loginUserRequest, bankAccount2.getId());
 
         // then
         assertEquals(204, response.getStatusCodeValue());
-        verify(bankAccountService).deleteBankAccount(loginUserRequest.userId(), id);
+        boolean exists = bankAccountRepository.findById(bankAccount2.getId()).isPresent();
+        assertThat(exists).isFalse();
     }
 
     @DisplayName("로그인 한 사용자가 자신의 선택한 계좌 정보를 삭제한다.")
     @Test
     void getBankAccount() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+
+        BankAccount bankAccount = setupBankAccount(user, "!23", "123", "123", false);
+        BankAccount bankAccount2 = setupBankAccount(user, "!234", "124", "1243", false);
+        bankAccountRepository.saveAll(List.of(bankAccount, bankAccount2));
 
         // when
         ResponseEntity<List<BankAccountGetResponse>> response = controller.getBankAccount(loginUserRequest);
 
         // then
         assertEquals(200, response.getStatusCodeValue());
-        verify(bankAccountService).getBankAccounts(loginUserRequest.userId());
+        assertEquals(response.getBody().size(), 2);
+        assertEquals(response.getBody().get(1).accountHolder(), "124");
     }
 }
--- athena/src/test/java/goorm/athena/domain/deliveryinfo/controller/DeliveryInfoControllerImplTest.java
+++ athena/src/test/java/goorm/athena/domain/deliveryinfo/controller/DeliveryInfoControllerImplTest.java
@@ -4,7 +4,9 @@ import goorm.athena.domain.deliveryinfo.DeliveryControllerIntegrationTestSupport
 import goorm.athena.domain.deliveryinfo.dto.req.DeliveryChangeStateRequest;
 import goorm.athena.domain.deliveryinfo.dto.req.DeliveryInfoRequest;
 import goorm.athena.domain.deliveryinfo.dto.res.DeliveryInfoResponse;
+import goorm.athena.domain.deliveryinfo.entity.DeliveryInfo;
 import goorm.athena.domain.user.entity.Role;
+import goorm.athena.domain.user.entity.User;
 import goorm.athena.global.jwt.util.LoginUserRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,8 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 class DeliveryInfoControllerImplTest extends DeliveryControllerIntegrationTestSupport {
 
@@ -21,58 +23,88 @@ class DeliveryInfoControllerImplTest extends DeliveryControllerIntegrationTestSu
     @Test
     void addDeliveryInfo() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
-        DeliveryInfoRequest request = new DeliveryInfoRequest("123", "123", "123");
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+        DeliveryInfoRequest request = new DeliveryInfoRequest("서울시 강남구", "01012345678", "홍길동");
 
         // when
         ResponseEntity<Void> response = controller.addDeliveryInfo(loginUserRequest, request);
 
         // then
         assertEquals(200, response.getStatusCodeValue());
-        verify(deliveryInfoService).addDeliveryInfo(loginUserRequest.userId(), request);
+
+        List<DeliveryInfo> all = deliveryInfoRepository.findAll();
+        assertEquals(1, all.size());
+        assertEquals("서울시 강남구", all.get(0).getZipcode());
     }
 
     @DisplayName("유저의 기존에 저장된 배송 정보와 선택한 배송 정보의 상태를 변경한다.")
     @Test
     void changeDeliveryInfoState() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
-        DeliveryChangeStateRequest request = new DeliveryChangeStateRequest(1L);
+        User user = setupUser("123123213", "123", "123", null);
+        DeliveryInfo deliveryInfo = new DeliveryInfo(user, "123", "123", "123", true);
+        DeliveryInfo deliveryInfo2 = new DeliveryInfo(user, "123", "123", "123", false);
+
+        userRepository.save(user);
+
+        deliveryInfoRepository.saveAll(List.of(deliveryInfo, deliveryInfo2));
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+        DeliveryChangeStateRequest request = new DeliveryChangeStateRequest(deliveryInfo2.getId());
 
         // when
         ResponseEntity<Void> response = controller.changeDeliveryInfoState(loginUserRequest, request);
 
         // then
         assertEquals(204, response.getStatusCodeValue());
-        verify(deliveryInfoService).changeDeliveryState(loginUserRequest.userId(), request.deliveryInfoId());
+        assertEquals(deliveryInfo2.getId(), deliveryInfoService.getPrimaryDeliveryInfo(user.getId()).getId());
     }
 
     @DisplayName("로그인 한 사용자가 자신의 선택한 배송 정보를 삭제한다.")
     @Test
     void deleteDeliveryInfo() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
-        Long id = 1L;
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+
+        DeliveryInfo deliveryInfo = new DeliveryInfo(user, "!23", "123", "123", false);
+        DeliveryInfo deliveryInfo2 = new DeliveryInfo(user, "!234", "124", "1243", false);
+        deliveryInfoRepository.saveAll(List.of(deliveryInfo, deliveryInfo2));
 
         // when
-        ResponseEntity<Void> response = controller.deleteDeliveryInfo(loginUserRequest, id);
+        ResponseEntity<Void> response = controller.deleteDeliveryInfo(loginUserRequest, deliveryInfo2.getId());
 
         // then
         assertEquals(204, response.getStatusCodeValue());
-        verify(deliveryInfoService).deleteDeliveryInfo(loginUserRequest.userId(), id);
+
+        boolean exists = deliveryInfoRepository.findById(deliveryInfo2.getId()).isPresent();
+        assertThat(exists).isFalse();
     }
 
     @DisplayName("로그인 한 사용자가 자신의 배송 정보들을 조회한다.")
     @Test
     void getDeliveryInfoList() {
         // given
-        LoginUserRequest loginUserRequest = new LoginUserRequest("123", 1L, Role.ROLE_USER);
+        User user = setupUser("123", "123", "123", null);
+        userRepository.save(user);
+        LoginUserRequest loginUserRequest = new LoginUserRequest("123", user.getId(), Role.ROLE_USER);
+
+        DeliveryInfo deliveryInfo = new DeliveryInfo(user, "!234", "124", "1243", false);
+        DeliveryInfo deliveryInfo2 = new DeliveryInfo(user, "!234", "124", "1243", false);
+        deliveryInfoRepository.save(deliveryInfo);
+        deliveryInfoRepository.save(deliveryInfo2);
 
         // when
         ResponseEntity<List<DeliveryInfoResponse>> response = controller.getDeliveryInfoList(loginUserRequest);
 
         // then
         assertEquals(200, response.getStatusCodeValue());
-        verify(deliveryInfoService).getMyDeliveryInfo(loginUserRequest.userId());
+        assertEquals(response.getBody().size(), 2);
+        assertEquals(response.getBody().get(1).zipcode(), "!234");
     }
 }
