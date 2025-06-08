754fbc174b26474041bd838d7e76335defdb6da5
goorm <rlarlgus300@gmail.com>
2025-06-08 13:52:43 +0900

    test : UserCoupon 컨트롤러 실 객체간 연결하는 방식으로 테스트 작성

---
+++ athena/src/test/java/goorm/athena/domain/userCoupon/util/UserCouponControllerIntegrationSupport.java
@@ -0,0 +1,55 @@
+package goorm.athena.domain.userCoupon.util;
+
+import goorm.athena.domain.coupon.entity.Coupon;
+import goorm.athena.domain.coupon.entity.CouponStatus;
+import goorm.athena.domain.coupon.repository.CouponRepository;
+import goorm.athena.domain.imageGroup.entity.ImageGroup;
+import goorm.athena.domain.user.entity.Role;
+import goorm.athena.domain.user.entity.User;
+import goorm.athena.domain.user.repository.UserRepository;
+import goorm.athena.domain.user.service.UserService;
+import goorm.athena.domain.userCoupon.controller.UserCouponController;
+import goorm.athena.domain.userCoupon.entity.Status;
+import goorm.athena.domain.userCoupon.entity.UserCoupon;
+import goorm.athena.domain.userCoupon.repository.UserCouponRepository;
+import goorm.athena.domain.userCoupon.service.UserCouponService;
+import goorm.athena.util.IntegrationControllerTestSupport;
+import goorm.athena.util.TestEntityFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public abstract class UserCouponControllerIntegrationSupport extends IntegrationControllerTestSupport {
+    @Autowired
+    protected UserCouponController controller;
+
+    @Autowired
+    protected UserCouponService userCouponService;
+
+    @Autowired
+    protected UserCouponRepository userCouponRepository;
+
+    @Autowired
+    protected CouponRepository couponRepository;
+
+    @Autowired
+    protected UserService userService;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+
+    protected User setupUser(String email, String password, String nickname, ImageGroup imageGroup) {
+        User user = TestEntityFactory.createUser(email, password, nickname, imageGroup, Role.ROLE_USER);
+        return user;
+    }
+
+    protected Coupon setupCoupon(String title, String content, int price, LocalDateTime startAt,
+                                 LocalDateTime endAt, LocalDateTime expiresAt, int stock, CouponStatus couponStatus){
+        return TestEntityFactory.createCoupon(title, content, price, startAt, endAt, expiresAt, stock, couponStatus);
+    }
+
+    protected UserCoupon setupUserCoupon(User user, Coupon coupon, Status status){
+        return TestEntityFactory.createUserCoupon(user, coupon, status);
+    }
+}
