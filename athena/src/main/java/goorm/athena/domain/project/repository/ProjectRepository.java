78d6bee025e6c972e819ba9e38ee1840de167833
Jay(김재훈) <jkim94937@gmail.com>
2025-06-09 13:54:05 +0900

    fix: ProjectRepository 인터페이스에 JpaRepository 및 JpaSpecificationExecutor 추가

--- athena/src/main/java/goorm/athena/domain/project/repository/ProjectRepository.java
+++ athena/src/main/java/goorm/athena/domain/project/repository/ProjectRepository.java
@@ -1,16 +1,18 @@
 package goorm.athena.domain.project.repository;
 
-import goorm.athena.domain.project.entity.Project;
-import goorm.athena.domain.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import goorm.athena.domain.project.entity.Project;
 import goorm.athena.domain.project.entity.ApprovalStatus;
-
-
+import goorm.athena.domain.user.entity.User;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
     @Query("SELECT p FROM Project p " +
