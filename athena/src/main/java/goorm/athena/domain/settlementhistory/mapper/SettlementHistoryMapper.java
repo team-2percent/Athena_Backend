9178841b05d8bd5c55f020ca329776b9d6dcb23e
yohan <rmvmf96@gmail.com>
2025-06-11 10:09:03 +0900

    fix : error 실행 수정

--- athena/src/main/java/goorm/athena/domain/settlementhistory/mapper/SettlementHistoryMapper.java
+++ athena/src/main/java/goorm/athena/domain/settlementhistory/mapper/SettlementHistoryMapper.java
@@ -4,18 +4,15 @@ import goorm.athena.domain.order.entity.Order;
 import goorm.athena.domain.settlement.entity.Settlement;
 import goorm.athena.domain.settlementhistory.entity.SettlementHistory;
 
-public class SettlementHistoryMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-    public static SettlementHistory toEntity(Settlement settlement, Order order,
-                                             long totalPrice, long platformFee, long pgFee, long vat, long amount) {
-        return SettlementHistory.builder()
-                .settlement(settlement)
-                .order(order)
-                .totalPrice(totalPrice)
-                .platformFee(platformFee)
-                .pgFee(pgFee)
-                .vat(vat)
-                .amount(amount)
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface SettlementHistoryMapper {
+
+    @Mapping(target = "order", source = "order")
+    SettlementHistory toEntity(Settlement settlement, Order order,
+                               long totalPrice, long platformFee, long pgFee, long vat, long amount);
 }
+
+
