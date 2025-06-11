d9c2c15f4f88b3a0986afcff66c104a8e15d6788
goorm <rlarlgus300@gmail.com>
2025-06-11 19:31:49 +0900

    ref : RefreshToken Mapper ModelStruct를 이용하여 매핑하도록 변경

--- athena/src/main/java/goorm/athena/domain/user/mapper/RefreshTokenMapper.java
+++ athena/src/main/java/goorm/athena/domain/user/mapper/RefreshTokenMapper.java
@@ -2,12 +2,6 @@ package goorm.athena.domain.user.mapper;
 
 import goorm.athena.domain.user.dto.response.RefreshTokenResponse;
 
-public class RefreshTokenMapper {
-
-    public static RefreshTokenResponse toRefreshTokenResponse(Long userId, String accessToken, String refreshToken){
-        return new RefreshTokenResponse(
-                userId,
-                accessToken,
-                refreshToken);
-    }
+public interface RefreshTokenMapper {
+    RefreshTokenResponse toRefreshTokenResponse(Long userId, String accessToken, String refreshToken);
 }
