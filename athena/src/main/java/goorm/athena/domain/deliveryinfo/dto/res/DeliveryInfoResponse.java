c36564e02217e0be75cbd5a380e9749d23d73467
goorm <rlarlgus300@gmail.com>
2025-06-12 12:39:18 +0900

    ref : DeliveryInfo의 조회 응답 Dto를 Mapstruct로 연결하는 방식으로 변경

--- athena/src/main/java/goorm/athena/domain/deliveryinfo/dto/res/DeliveryInfoResponse.java
+++ athena/src/main/java/goorm/athena/domain/deliveryinfo/dto/res/DeliveryInfoResponse.java
@@ -1,21 +1,9 @@
 package goorm.athena.domain.deliveryinfo.dto.res;
 
-import goorm.athena.domain.deliveryinfo.entity.DeliveryInfo;
-
 public record DeliveryInfoResponse(
         Long id,
         String zipcode,
         String address,
         String detailAddress,
         boolean isDefault
-) {
-    public static DeliveryInfoResponse from(DeliveryInfo info) {
-        return new DeliveryInfoResponse(
-                info.getId(),
-                info.getZipcode(),
-                info.getAddress(),
-                info.getDetailAddress(),
-                info.isDefault()
-        );
-    }
-}
+) { }
--- athena/src/main/java/goorm/athena/domain/deliveryinfo/mapper/DeliveryInfoMapper.java
+++ athena/src/main/java/goorm/athena/domain/deliveryinfo/mapper/DeliveryInfoMapper.java
@@ -1,13 +1,18 @@
 package goorm.athena.domain.deliveryinfo.mapper;
 
+import goorm.athena.domain.deliveryinfo.dto.res.DeliveryInfoResponse;
 import goorm.athena.domain.deliveryinfo.entity.DeliveryInfo;
 import goorm.athena.domain.user.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DeliveryInfoMapper {
 
     @Mapping(target = "user", source = "user")
     DeliveryInfo toEntity(User user, String zipcode, String address, String detailAddress, boolean isDefault);
+
+    List<DeliveryInfoResponse> toGetResponse(List<DeliveryInfo> deliveryInfo);
 }
