ace0e9d3207c48fffacdb2106be1e79d034d05dc
Hyerim Jeon <86788892+JeonHyerim86@users.noreply.github.com>
2025-06-12 10:43:15 +0900

    ref: Image Mapper Mapstruct 방식으로 변경

--- athena/src/main/java/goorm/athena/domain/image/mapper/ImageMapper.java
+++ athena/src/main/java/goorm/athena/domain/image/mapper/ImageMapper.java
@@ -3,17 +3,13 @@ package goorm.athena.domain.image.mapper;
 import goorm.athena.domain.image.dto.req.ImageCreateRequest;
 import goorm.athena.domain.image.entity.Image;
 import goorm.athena.domain.imageGroup.entity.ImageGroup;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-public class ImageMapper {
-    // ImageCreatRequest(Dto) -> Image Entity
-    public static Image toEntity(ImageCreateRequest request, ImageGroup imageGroup, Long imageIndex) {
-        return Image.builder()
-                .imageGroup(imageGroup)
-                .fileName(request.fileName())
-                .originalUrl(request.originalUrl())
-                .fileType(request.fileType())
-                .imageIndex(imageIndex)
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface ImageMapper {
 
+    @Mapping(target = "imageGroup", source = "imageGroup")
+    @Mapping(target = "imageIndex", source = "imageIndex")
+    Image toEntity(ImageCreateRequest request, ImageGroup imageGroup, Long imageIndex);
 }
