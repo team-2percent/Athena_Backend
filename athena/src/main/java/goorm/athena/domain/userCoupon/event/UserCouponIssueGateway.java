fb0eada69b5018a27d3c49c52ee48d6a8728590a
goorm <rlarlgus300@gmail.com>
2025-06-22 01:55:45 +0900

    fix : 쿠폰 에러 발생으로 롤백을 위한 보상 트랜잭션 실행 시 잘못된 RBucket 사용으로 제대로 재고 롤백이 되지 않던 점 수정

--- athena/src/main/java/goorm/athena/domain/userCoupon/event/UserCouponIssueGateway.java
+++ athena/src/main/java/goorm/athena/domain/userCoupon/event/UserCouponIssueGateway.java
@@ -4,11 +4,13 @@ import goorm.athena.domain.coupon.event.CouponRollbackEvent;
 import goorm.athena.domain.userCoupon.infra.UserCouponStockOperation;
 import goorm.athena.domain.userCoupon.service.UserCouponCommandService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserCouponIssueGateway {
 
     private final UserCouponCommandService userCouponCommandService;
@@ -19,14 +21,18 @@ public class UserCouponIssueGateway {
         try {
             userCouponCommandService.issueUserCoupon(userId, couponId);
         } catch (Exception e){
+            log.error("[쿠폰 발급 실패] userId: {}, couponId: {}, 원인: {}", userId, couponId, e.getMessage(), e);
             try{
                 userCouponStockOperation.removeUserFromIssuedSet(couponId, userId);
                 if(luaResult == 1 || luaResult == 2){
-                    userCouponStockOperation.rollbackRedisStock(couponId);
+                    eventPublisher.publishEvent(new CouponRollbackEvent(couponId));
+                  //  throw new RuntimeException("유저 쿠폰 롤백 중 에러 발생");
                 }
             } catch (Exception rollbackEx) {
-                eventPublisher.publishEvent(new CouponRollbackEvent(couponId));
+                log.error("[Redis 롤백 실패] couponId: {}, rollbackEx: {}", couponId, rollbackEx.getMessage(), rollbackEx);
+                // TODO: 알림 시스템 구현 시 아래에 관리자 알림 기능 추가
             }
+            throw new RuntimeException("쿠폰 발급 중 예외 및 롤백 중 실패 발생", e);
         }
     }
 }
