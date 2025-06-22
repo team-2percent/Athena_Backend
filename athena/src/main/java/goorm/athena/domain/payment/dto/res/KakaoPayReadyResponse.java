3555384c56bfde93aedcd0de0d916c65097b7e39
yohan <rmvmf96@gmail.com>
2025-06-23 04:33:43 +0900

    feat: 제이미터 테스트를 위해서 order id 응답값 추가

--- athena/src/main/java/goorm/athena/domain/payment/dto/res/KakaoPayReadyResponse.java
+++ athena/src/main/java/goorm/athena/domain/payment/dto/res/KakaoPayReadyResponse.java
@@ -2,5 +2,7 @@ package goorm.athena.domain.payment.dto.res;
 
 public record KakaoPayReadyResponse(
         String next_redirect_pc_url,
-        String tid
+        String tid,
+        Long orderId
+
 ) {}
