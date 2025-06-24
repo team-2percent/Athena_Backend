b247c4cb8de96e1228bab5d9096061ebdbb77240
yohan <rmvmf96@gmail.com>
2025-06-24 14:11:20 +0900

    fix : 중복 코드 제거

--- athena/src/main/java/goorm/athena/AthenaApplication.java
+++ athena/src/main/java/goorm/athena/AthenaApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableAsync
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 public class AthenaApplication {
 
 	public static void main(String[] args) {
