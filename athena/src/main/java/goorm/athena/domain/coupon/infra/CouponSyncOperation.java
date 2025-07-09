babcc1bb8d942ccbc7258230d2462a8b78b934fe
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-07-10 04:22:13 +0900

    perf : 쿠폰 재고 동기 과정에서 오류 발생 시 재시도 로직 추가
    쿠폰 재고 동기 과정에서 오류 발생 시 Spring 단에서 재시도하는 로직 추가
    
    만약, 동기에 실패했다면 어느 쿠폰이 동기에 실패했는지 로그로 출력한 후 해당 쿠폰을 사용하지 못하도록 상태 변경

--- athena/src/main/java/goorm/athena/domain/coupon/infra/CouponSyncOperation.java
+++ athena/src/main/java/goorm/athena/domain/coupon/infra/CouponSyncOperation.java
@@ -5,12 +5,17 @@ import goorm.athena.domain.coupon.entity.CouponStatus;
 import goorm.athena.domain.coupon.repository.CouponRepository;
 import goorm.athena.domain.coupon.service.CouponQueryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.StringCodec;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CouponSyncOperation {
@@ -19,6 +24,11 @@ public class CouponSyncOperation {
     private final CouponQueryService couponQueryService;
     private final CouponRepository couponRepository;
 
+    @Retryable(
+            value = { RuntimeException.class},
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     @Transactional
     public void syncCouponStock(Long couponId){
         Coupon coupon = couponQueryService.getCoupon(couponId);
@@ -40,5 +50,25 @@ public class CouponSyncOperation {
         }
 
         couponRepository.save(coupon);
+    }
+
+    @Recover
+    public void recover(RuntimeException e, Long couponId) {
+        String metaKey = "coupon_meta_" + couponId;
+        RMap<String, String> couponMeta = redissonClient.getMap(metaKey, StringCodec.INSTANCE);
+
+        String totalKey = couponMeta.get("total");
+        String usedKey = couponMeta.get("used");
+
+        log.error("쿠폰 재고 동기화 최종 실패: couponId={}, 총 재고={}, 사용량={}, 에러={}",
+                couponId, totalKey, usedKey, e.getMessage());
+
+        // 쿠폰 상태를 SYNC_FAILED로 변경하여 서비스에서 사용 금지 처리
+        log.info("recover - 쿠폰 상태 변경 시작");
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow(() -> new RuntimeException("쿠폰을 찾을 수 없습니다: " + couponId));
+        coupon.syncFailed();
+        couponRepository.save(coupon);
+        log.info("recover - 쿠폰 상태 변경 완료");
     }
 }
